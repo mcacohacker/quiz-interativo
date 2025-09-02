e28a1461a129832f165d54fc47657cdf4050e001
mcacohacker <yasmim.isabeli.costa@escola.pr.gov.br>
2025-09-02 11:05:44 -0300

    Update perguntas.js

---
+++ js/perguntas.js
@@ -0,0 +1,10 @@
+const perguntas = {
+  {
+  enunciado: "pergunta 1", //a pergunta
+    alternativas: [
+    {
+texto: "nike", //primeira alternativa
+      afirmacao: [
+       "voce tem bom gosto", //comentario para a resposta
+        "afirmaçao 2"       //segundo comentario
+        
